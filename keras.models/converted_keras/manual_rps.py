cfc8ee1753ebfcf0e4321098fd11311bca9d3ad7
OllieMountier <olliemountier@btinternet.com>
2023-01-17 17:50:48 +0000

    adds manual_rps.py to repo

--- keras.models/converted_keras/manual_rps.py
+++ keras.models/converted_keras/manual_rps.py
@@ -15,7 +15,32 @@ def get_user_choice():
             break
         else:
             print("Please enter a valid guess: ")
-            
+
+def get_winner():
+    global computer_choice
+    global user_choice
+    
+    if computer_choice == 'rock' and user_choice == 'paper' or \
+        computer_choice == 'paper' and user_choice == 'rock' or\
+        computer_choice == 'scissors' and user_choice == 'paper':
+        print ("Computer wins")
+        
+    ##user wins
+    elif user_choice == 'rock' and computer_choice == "scissors" or \
+        user_choice == 'paper' and computer_choice == "rock" or \
+        user_choice == 'scissors' and computer_choice == "paper":
+        print ("User wins")
+        
+    ##tie
+    else:
+        print ("Its a tie")
+
+def play():
+    get_computer_choice()
+    get_user_choice()
+    get_winner()
+
+play()
 
     
 
