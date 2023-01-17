5b2d2a78c601b39652242ca9e01363de40c6d153
OllieMountier <olliemountier@btinternet.com>
2023-01-17 18:49:00 +0000

    commits file

--- manual_rps.py
+++ manual_rps.py
@@ -10,6 +10,19 @@ def get_user_choice():
     user_choice = input("Please enter your guess: ")
     return user_choice
 
-get_computer_choice()
-get_user_choice()
+def get_winner(computer_choice, user_choice):
+    if computer_choice == 'Rock' and user_choice == 'Paper' or \
+        computer_choice == 'Paper' and user_choice == 'Rock' or\
+        computer_choice == 'Scissors' and user_choice == 'Paper':
+        print ("You lost!")
+        
+    ##user wins
+    elif user_choice == 'Rock' and computer_choice == 'Scissors' or \
+        user_choice == 'Paper' and computer_choice == 'Rock' or \
+        user_choice == 'Scissors' and computer_choice == 'Paper':
+        print ("You win!")
+        
+    ##tie
+    else:
+        print ("Its a tie")
 
