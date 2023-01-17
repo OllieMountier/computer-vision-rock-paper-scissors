8b107ca76eba414f6b5c36dbd29358dec2ec49c3
OllieMountier <olliemountier@btinternet.com>
2023-01-17 18:42:49 +0000

    commits file

--- manual_rps.py
+++ manual_rps.py
@@ -8,7 +8,7 @@ def get_computer_choice():
 
 def get_user_choice():
     user_choice = input("Please enter your guess: ")
-    print(user_choice)
+    return user_choice
 
 get_computer_choice()
 get_user_choice()
