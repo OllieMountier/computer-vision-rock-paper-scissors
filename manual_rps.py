5da399019ea3a6803d9a96d0703c69e17e35dcc5
OllieMountier <olliemountier@btinternet.com>
2023-01-18 17:39:30 +0000

    commits file to repo

--- manual_rps.py
+++ manual_rps.py
@@ -9,18 +9,21 @@ def get_user_choice():
     user_choice = input("Please enter your guess: ")
     return user_choice
 
+user_choice = get_user_choice()
+computer_choice = get_computer_choice()
+
 def get_winner(computer_choice, user_choice):
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
-    if computer_choice == 'Rock' and user_choice == 'Scissors' or computer_choice == 'Paper' and user_choice == 'Rock' or computer_choice == 'Scissors' and user_choice == 'Paper':
+    if ((computer_choice == 'Rock') and (user_choice == 'Scissors')) or ((computer_choice == 'Paper') and (user_choice == 'Rock')) or ((computer_choice == 'Scissors') and (user_choice == 'Paper')):
         print("You won")
     
-    elif computer_choice == 'Rock' and user_choice == 'Paper' or computer_choice == 'Paper' and user_choice == 'Scissors' or computer_choice == 'Scissors' and user_choice == 'Rock':
+    elif ((computer_choice == 'Rock') and (user_choice == 'Paper')) or ((computer_choice == 'Paper') and (user_choice == 'Scissors')) or ((computer_choice == 'Scissors') and (user_choice == 'Rock')):
         print("You lost")
 
     else:
         print("It is a tie!")
         
-get_winner(computer_choice = get_computer_choice, user_choice = get_user_choice)
+get_computer_choice()
+get_user_choice()
+get_winner(computer_choice, user_choice)
 
 
